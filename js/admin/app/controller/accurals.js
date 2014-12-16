445c0c2c37ef622de42615fbbeea691939e7e71b
Pavel Perminov <pavel.perminov.23@gmail.com>
2014-12-16 09:50:55 +0300

    asd

--- js/admin/app/controller/accurals.js
+++ js/admin/app/controller/accurals.js
@@ -86,35 +86,6 @@ Ext.define('Indi.controller.accurals', {
                         return this.grid.headerCt.getGridColumns().r(dataIndex, 'dataIndex').renderer(value);
                     }
                 });
-            },
-            gridColumnA: function() {
-                var me = this, plainColumnA = me.callParent(), groupedColumnA = Ext.clone(plainColumnA), groups;
-
-                groupedColumnA = me.gridColumnAGroup(groupedColumnA, [
-                    {text: 'Фиксированный тариф', start: 'fixedTariffId', span: 3},
-                    {text: 'Прогрессивный тариф', start: 'floatTariffId', span: 3},
-                    {text: 'Руководительский тариф', start: 'chiefTariffId', span: 3},
-                    {text: 'Кровь', start: 'bloodQty', span: 2},
-                    {text: 'Мазок', start: 'smearQty', span: 2},
-                    {text: 'Суммы', start: 'totalSum', span: 3},
-                ]);
-
-                return groupedColumnA;
-            },
-
-            gridColumnAGroup: function(columnA, groupA) {
-                var inspan;
-                for (var i = 0; i < groupA.length; i++) if (groupA[i].span > 1)
-                    for (var j = 0; j < columnA.length; j++) if (columnA[j].dataIndex == groupA[i].start) {
-                        inspan = columnA.splice(j, groupA[i].span);
-                        columnA.splice(j, 0, {
-                            text: groupA[i].text,
-                            columns: inspan
-                        });
-                        break;
-                    }
-
-                return columnA;
             }
         }
     }
--- js/admin/app/controller/doctorBasket.js
+++ js/admin/app/controller/doctorBasket.js
@@ -55,32 +55,6 @@ Ext.define('Indi.controller.doctorBasket', {
                         return this.grid.headerCt.getGridColumns().r(dataIndex, 'dataIndex').renderer(value);
                     }
                 });
-            },
-            gridColumnA: function() {
-                var me = this, plainColumnA = me.callParent(), groupedColumnA = Ext.clone(plainColumnA), groups;
-
-                groupedColumnA = me.gridColumnAGroup(groupedColumnA, [
-                    {text: 'Фиксированный тариф', start: 'fixedTariffId', span: 3},
-                    {text: 'Прогрессивный тариф', start: 'floatTariffId', span: 3},
-                    {text: 'Суммы', start: 'totalSum', span: 3},
-                ]);
-
-                return groupedColumnA;
-            },
-
-            gridColumnAGroup: function(columnA, groupA) {
-                var inspan;
-                for (var i = 0; i < groupA.length; i++) if (groupA[i].span > 1)
-                    for (var j = 0; j < columnA.length; j++) if (columnA[j].dataIndex == groupA[i].start) {
-                        inspan = columnA.splice(j, groupA[i].span);
-                        columnA.splice(j, 0, {
-                            text: groupA[i].text,
-                            columns: inspan
-                        });
-                        break;
-                    }
-
-                return columnA;
             }
         }
     }

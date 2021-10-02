b48a6e16cb3da3e14ba91ee1b8216160be18122c
Pavel Perminov <pavel.perminov.23@gmail.com>
2021-10-02 20:20:45 +0300

    added application/lang/ui.php

---
+++ application/lang/ui.php
@@ -0,0 +1,17 @@
+<?php
+field('action', 'title')->toggleL10n('qy', 'ru', false);
+field('admin', 'title')->toggleL10n('qy', 'ru', false);
+field('field', 'title')->toggleL10n('qy', 'ru', false);
+field('entity', 'title')->toggleL10n('qy', 'ru', false);
+field('field', 'tooltip')->toggleL10n('qy', 'ru', false);
+field('grid', 'summaryText')->toggleL10n('qy', 'ru', false);
+field('notice', 'title')->toggleL10n('qy', 'ru', false);
+field('notice', 'tooltip')->toggleL10n('qy', 'ru', false);
+field('notice', 'tplIncSubj')->toggleL10n('qy', 'ru', false);
+field('notice', 'tplIncBody')->toggleL10n('qy', 'ru', false);
+field('notice', 'tplDecSubj')->toggleL10n('qy', 'ru', false);
+field('notice', 'tplDecBody')->toggleL10n('qy', 'ru', false);
+field('notice', 'tplEvtSubj')->toggleL10n('qy', 'ru', false);
+field('notice', 'tplEvtBody')->toggleL10n('qy', 'ru', false);
+field('profile', 'title')->toggleL10n('qy', 'ru', false);
+field('section', 'title')->toggleL10n('qy', 'ru', false);

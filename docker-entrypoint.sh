4fc76780cfc290fca98fb19c6bbb19392d10de2a
Pavel Perminov <pavel.perminov.23@gmail.com>
2022-05-23 19:20:03 +0300

    docker: ws-proxy port ENV is set in docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for f in application/ws.* ; do rm "$f" ; done
 rm debug.txt
+export WSP=$(grep -Pzo "(?s)\[ws\].*?port.*?\K[0-9]+" application/config.ini | tr -d '\0')
 chmod 0777 . application
 service rabbitmq-server start
 php vendor/indi-engine/system/application/ws.php > /dev/null &

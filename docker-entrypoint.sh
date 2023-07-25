e55eb4701f775e46222fc1c0096bcbd88c88522c
Pavel Perminov <pavel.perminov.23@gmail.com>
2023-07-25 12:45:01 +0300

    1.docker-entrypoint.sh  - mysql and rabbitmq services are started only    if $(MYSQL|RABBITMQ)_HOST variables are NOT given 2..htaccess  - added SetEnvIf ^ RABBITMQ_HOST= RABBITMQ_HOST=localhost  - amended ws-proxy rule to RewriteRule ^/?(.*) "ws://%{ENV:RABBITMQ_HOST}:15674/ws" [P] 3.created  - compose/    - rabbitmq/      - enabled_plugins      - rabbitmq.conf    - Dockerfile  - docker-compose.yml 4.composer.lock updated:
      vendor/indi-engine/system:
    
        -----2023-07-25 12:08:01-----
    
        index.php:
         hosts are spoofed for mysql and rabbitmq if given by environment variables
    
        -----2023-07-24 21:27:40-----
    
        1.RealtimeController->binlogService(): added rabbitmq_host-param
        2.library/Indi/Db.php: increased timeout for waiting mysql-container ready
        3.added sql/maxwell.sql
         - maxwell-deamon's specific GRANTs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-service rabbitmq-server start
-/usr/local/bin/docker-entrypoint.sh mysqld &
+[ -z "$RABBITMQ_HOST" ] && service rabbitmq-server start
+[ -z "$MYSQL_HOST" ] && /usr/local/bin/docker-entrypoint.sh mysqld &
 /sbin/runuser www-data -s /bin/bash -c 'if [[ -f "debug.txt" ]] ; then rm debug.txt ; fi'
 /sbin/runuser www-data -s /bin/bash -c 'if [[ ! -d "log" ]] ; then mkdir log ; fi'
 /sbin/runuser www-data -s /bin/bash -c "php indi -d realtime/closetab"

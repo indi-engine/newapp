e643f8a5451e3c62bbfa676248e87b21abb98f46
Pavel Perminov <pavel.perminov.23@gmail.com>
2023-07-25 18:05:31 +0300

    1.compose/Dockerfile  - added ncat to the list of utils to be installed  - CR trimmed from vendor/..../tkill 2.docker-entrypoint.sh  - if $RABBITMQ_HOST is given - added script to wait for RabbitMQ ready on $RABBITMQ_HOST on $port    - $port is picked from application/config.ini's [rabbitmq]->port    - else rabbitmq service started right here but in background  - if $MYSQL_HOST is not given - mysql service started right here but in background as well  - added check for Apache's old pid-file kept from previous start of container,    file is removed to prevent 'httpd (pid 95) already running' error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
-[ -z "$RABBITMQ_HOST" ] && service rabbitmq-server start
-[ -z "$MYSQL_HOST" ] && /usr/local/bin/docker-entrypoint.sh mysqld &
+
+# Remove debug.txt file, if exists, and create log/ directory if not exists
 /sbin/runuser www-data -s /bin/bash -c 'if [[ -f "debug.txt" ]] ; then rm debug.txt ; fi'
 /sbin/runuser www-data -s /bin/bash -c 'if [[ ! -d "log" ]] ; then mkdir log ; fi'
+
+# If $RABBITMQ_HOST is given
+if [ -n "$RABBITMQ_HOST" ]; then
+
+  # Extract the RabbitMQ port from application/config.ini
+  port=$(grep -Pzo "(?s)\[rabbitmq\].*?port.*?\K[0-9]+" application/config.ini | tr -d '\0')
+
+  # Print rabbitmq port found
+  echo "RabbitMQ port is $port"
+
+  # Wait for RabbitMQ to be ready in separate container
+  while ! ncat -z $RABBITMQ_HOST $port >/dev/null 2>&1; do echo "Waiting for RabbitMQ..." && sleep 5; done
+
+# Else start rabbitmq right here
+else service rabbitmq-server start; fi
+
+# If $MYSQL_HOST is not given - start mysql right here as well
+[ -z "$MYSQL_HOST" ] && /usr/local/bin/docker-entrypoint.sh mysqld &
+
+# Start php background processes
 /sbin/runuser www-data -s /bin/bash -c "php indi -d realtime/closetab"
 /sbin/runuser www-data -s /bin/bash -c "php indi realtime/maxwell/enable"
+
+# Apache pid-file
+pid_file="/var/run/apache2/apache2.pid"
+
+# Remove pid-file, if kept from previous start of apache container
+if [ -f "$pid_file" ]; then rm "$pid_file" && echo "Apache old pid-file removed"; fi
+
+# Start apache process
 apachectl -D FOREGROUND

810b05364cd5569d03ccb8520a7c958cb73646d4
Pavel Perminov <pavel.perminov.23@gmail.com>
2023-09-07 14:11:53 +0300

    1.htaccess  - fixed RABBITMQ_HOST-variable check  - small cleanup 2.docker-compose.yml  - added services.apache.depends_on.(mysql|rebbitmq).condition: service_healthy  - added services.(mysql|rabbitmq).healthcheck 3.compose/Dockerfile  - amended chown-command to be applied to '/var/www'-dir rather than to '/var/www/html'-dir  - added 'RUN usermod -s /bin/bash www-data' 4.docker-entrypoint.sh  - removed rabbitmq-check

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,20 +4,8 @@
 /sbin/runuser www-data -s /bin/bash -c 'if [[ -f "debug.txt" ]] ; then rm debug.txt ; fi'
 /sbin/runuser www-data -s /bin/bash -c 'if [[ ! -d "log" ]] ; then mkdir log ; fi'
 
-# If $RABBITMQ_HOST is given
-if [ -n "$RABBITMQ_HOST" ]; then
-
-  # Extract the RabbitMQ port from application/config.ini
-  port=$(grep -Pzo "(?s)\[rabbitmq\].*?port.*?\K[0-9]+" application/config.ini | tr -d '\0')
-
-  # Print rabbitmq port found
-  echo "RabbitMQ port is $port"
-
-  # Wait for RabbitMQ to be ready in separate container
-  while ! ncat -z $RABBITMQ_HOST $port >/dev/null 2>&1; do echo "Waiting for RabbitMQ..." && sleep 5; done
-
-# Else start rabbitmq right here
-else service rabbitmq-server start; fi
+# If $RABBITMQ_HOST is not given - start rabbitmq server right here
+[ -z "$RABBITMQ_HOST" ] && service rabbitmq-server start
 
 # If $MYSQL_HOST is not given - start mysql right here as well
 [ -z "$MYSQL_HOST" ] && /usr/local/bin/docker-entrypoint.sh mysqld &

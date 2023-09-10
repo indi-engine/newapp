a5f71021a1a6d635d743407d5b7d9124267d2e5f
Pavel Perminov <pavel.perminov.23@gmail.com>
2023-09-10 19:57:38 +0300

    docker-compose setup: 1.compose/mysql/docker-entrypoint.sh: added support for whitespace-separated dumps paths/URLs 2.env.dist: MYSQL_DUMP: added comments about support for whitespace-separated dumps paths/URLs

--- compose/mysql/docker-entrypoint.sh
+++ compose/mysql/docker-entrypoint.sh
@@ -8,7 +8,7 @@ if ! command -v wget &>/dev/null; then
   apt-get update && apt-get install -y wget
 
   # Print which dump is going to be imported
-  echo "MYSQL_DUMP is $MYSQL_DUMP";
+  echo "MYSQL_DUMP is '$MYSQL_DUMP'";
 
   # Change dir
   cd /docker-entrypoint-initdb.d
@@ -16,32 +16,75 @@ if ! command -v wget &>/dev/null; then
   # Array of other sql files to be imported
   declare -a import=("maxwell.sql")
 
-  # If MYSQL_DUMP is an URL
-  if [[ $MYSQL_DUMP == http* ]]; then
+  # Split MYSQL_DUMP on whitespace into an array
+  IFS=' ' read -ra dumpA <<< "$MYSQL_DUMP"
 
-    # Download it right here
-    echo "Fetching MySQL dump from $MYSQL_DUMP..." && wget --no-check-certificate "$MYSQL_DUMP"
+  # Empty dumps counter
+  empty=0
 
-  # Else assume it's a local path pointing to some file inside
-  # /docker-entrypoint-initdb.d/custom/ directory mapped as a volume
-  # from sql/ directory on the docker host machine
-  else
+  # Downloaded/copied dumps counter, to be used in filename prefix to preserve import order
+  prefix=0
 
-    # Shortcut
-    dumpfile="custom/$MYSQL_DUMP"
+  # Foreach dump
+  for dump in "${dumpA[@]}"; do
 
-    # If that local path to sql-dump file really exists in custom/ and that file is NOT empty
-    if [ -f $dumpfile ] && [ -s $dumpfile ]; then
+    # If dump is an URL
+    if [[ $dump == http* ]]; then
 
-      # Copy that file here for it to be imported, as files from subdirectories are ignored while import
-      cp $dumpfile . && echo "File /docker-entrypoint-initdb.d/$dumpfile copied to the level up"
+      # Extract the filename from the URL and add counter-prefix 
+      name="$prefix-$(basename "$dump")"
 
-    # Else
+      # Download it right here
+      echo "Fetching remote MySQL dump from '$dump' into local '$name' ..." && wget --no-check-certificate -O "$name" "$dump"
+
+      # Increment saved dumps counter
+      prefix=$((prefix + 1))
+
+    # Else assume it's a local path pointing to some file inside
+    # /docker-entrypoint-initdb.d/custom/ directory mapped as a volume
+    # from sql/ directory on the docker host machine
     else
 
-      # Use system.sql
-      import+=("system.sql") && echo "The file specified in \$MYSQL_DUMP does not exist or is empty, so using system.sql"
+      # Shortcut
+      local="custom/$dump"
+
+      # If that local dump file really exists in sql/ directory on host machine
+      # e.g exists  in custom/ directory in mysql-container due to bind volume mapping
+      if [ -f "$local" ]; then
+
+        # If that local dump file is not empty
+        if [ -s "$local" ]; then
+
+          # Copy that file here for it to be imported, as files from subdirectories are ignored while import
+          cp "$local" "$prefix-$dump" && echo "File '/docker-entrypoint-initdb.d/$local' copied to the level up"
+
+          # Increment saved dumps counter
+          prefix=$((prefix + 1))
+
+        # Else
+        else
+
+          # Print warning
+          echo "[Warning] Local SQL dump file '$dump' is empty"
+
+          # Increment empty dumps counter
+          empty=$((empty + 1))
+        fi
+
+      # Else if that local dump file does not really exist
+      else
+
+        # Print error an exit
+        echo "SQL dump file '$dump' is not found!" && exit 1
+      fi
     fi
+  done
+
+  # If no dump(s) were given by MYSQL_DUMP-variable or all are empty
+  if [ $(( ${#dumpA[@]} - empty )) -eq 0 ]; then
+
+    # Use system.sql
+    import+=("system.sql") && echo "The file(s) specified by \$MYSQL_DUMP does not exist or is empty, so using system.sql"
   fi
 
   # Download the SQL dump files
@@ -50,7 +93,10 @@ if ! command -v wget &>/dev/null; then
     echo "Fetching from $url..." && wget "$url"
   done
 
-  # Append touch-command to create empty '/var/lib/mysql/init.done'-file after init is done to use in healthcheck
+  # Spoof mysql user name inside maxwell.sql, if need
+  [[ $MYSQL_USER != "custom" ]] && sed -i "s~custom~$MYSQL_USER~" maxwell.sql
+
+  # Append touch-command to create an empty '/var/lib/mysql/init.done'-file after init is done to use in healthcheck
   sed -i 's~Ready for start up."~&\n\t\t\ttouch /var/lib/mysql/init.done~' /usr/local/bin/docker-entrypoint.sh
 fi
 

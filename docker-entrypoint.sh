1063d5f209ee9a19363bfef1e15b9c31191bd470
Pavel Perminov <pavel.perminov.23@gmail.com>
2024-06-29 14:22:32 +0300

    docker-compose setup updated

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,6 +17,9 @@ run='/sbin/runuser '$user' -s /bin/bash -c'
 if [[ ! -z "$GIT_COMMIT_NAME"   && -z $($run 'git config user.name')  ]]; then $run 'git config --global user.name  "$GIT_COMMIT_NAME"' ; fi
 if [[ ! -z "$GIT_COMMIT_EMAIL"  && -z $($run 'git config user.email') ]]; then $run 'git config --global user.email "$GIT_COMMIT_EMAIL"'; fi
 
+# Setup git filemode
+$run 'git config --global core.filemode false'
+
 # Remove debug.txt file, if exists, and create log/ directory if not exists
 $run 'if [[ -f "debug.txt" ]] ; then rm debug.txt ; fi'
 $run 'if [[ ! -d "log" ]] ; then mkdir log ; fi'
@@ -42,6 +45,24 @@ if [ -f "$pid_file" ]; then rm "$pid_file" && echo "Apache old pid-file removed"
 
 # Copy 'mysql' and 'mysqldump' binaries to /usr/bin, to make it possible to restore/backup the whole database as sql-file
 cp /usr/bin/mysql_client_binaries/* /usr/bin/
+
+# Obtain Let's Encrypt certificate, if LE_DOMAIN env is given:
+# 1.Start apache in background to make certbot challenge possible
+# 2.Obtain certificate
+# 3.Stop apache in backgrond
+# 4.Setup cron job for certificate renewal check
+if [[ ! -z "$LETS_ENCRYPT_DOMAIN" ]]; then
+  service apache2 start
+  certbot --apache -n -d $LETS_ENCRYPT_DOMAIN -m $GIT_COMMIT_EMAIL --agree-tos -v
+  service apache2 stop
+  echo "0 */12 * * * certbot renew" | crontab -
+fi
+
+# Setup crontab
+env | grep -E "(MYSQL|RABBITMQ)_HOST|GIT_COMMIT_(NAME|EMAIL)|DOC" >> /etc/environment
+jobs='compose/crontab'
+sed -i "s~\$DOC~$DOC~" $jobs && crontab -u $user $jobs && $run 'git checkout '$jobs
+service cron start
 
 # Start apache process
 echo "Apache started" && apachectl -D FOREGROUND

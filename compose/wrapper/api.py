4da8adee9ae1232e6547500ddc20c22821f34c30
Pavel Perminov <pavel.perminov.23@gmail.com>
2025-07-22 11:48:23 +0300

    compose/wrapper/api.py: debug code added

--- compose/wrapper/api.py
+++ compose/wrapper/api.py
@@ -147,7 +147,7 @@ def bash_stream(
             if scenario == 4:
                 bytes = "Doing it. Please wait for 'Reconnected' message in the bottom-left corner."
             elif scenario != 5:
-                bytes = "Doing it. Please wait for 'Proceed to re-login' prompt and click OK there then." + scenario
+                bytes = "Doing it. Please wait for 'Proceed to re-login' prompt and click OK there then. " + scenario
     else:
         bytes = ''
 

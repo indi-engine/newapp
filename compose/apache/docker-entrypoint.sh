46cf29c125d0c4b1d0fe5b1a8ba05d801b019bd5
Pavel Perminov <pavel.perminov.23@gmail.com>
2025-12-11 18:56:30 +0300

    compose/apache/docker-entrypoint.sh: added --expand flag to certbot-command

--- compose/apache/docker-entrypoint.sh
+++ compose/apache/docker-entrypoint.sh
@@ -37,7 +37,7 @@ if [[ ! -z "$LETS_ENCRYPT_DOMAIN" ]]; then
   domainsArg=$(echo "$LETS_ENCRYPT_DOMAIN" | sed 's/ / -d /g')
   if [[ ! -z "$LETS_ENCRYPT_NOTIFY" ]];  then emailArg="-m $LETS_ENCRYPT_NOTIFY"
   elif [[ "$APP_ENV" != "production" ]]; then emailArg="--register-unsafely-without-email"; fi
-  certbot --apache -n -d $domainsArg $emailArg --agree-tos -v --logs-dir "$logs"
+  certbot --expand --apache -n -d $domainsArg $emailArg --agree-tos -v --logs-dir "$logs"
   service apache2 stop
   echo "0 */12 * * * certbot renew --logs-dir $logs" | crontab -
 

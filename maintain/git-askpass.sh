c67bd42a3d534ed470f3a576229b8a79138f330a
Pavel Perminov <pavel.perminov.23@gmail.com>
2026-03-05 10:27:29 +0300

    maintain/git-askpass.sh: fixed GH_TOKEN presence detection

--- maintain/git-askpass.sh
+++ maintain/git-askpass.sh
@@ -2,7 +2,7 @@
 
 # If GH_TOKEN env is set, it means we're here due to git_askpass() calls
 # called by 'source update' and/or 'source backup' command, so print that env
-if env | grep -q GH_TOKEN; then
+if env | grep -q GH_TOKEN=; then
   echo "${GH_TOKEN:-}"
 
 # Else it means we're here due to manual 'git push' command, or another command that

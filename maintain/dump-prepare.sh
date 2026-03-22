7dce724e308fda5e7deac1e9e7718125961ec6bc
Pavel Perminov <pavel.perminov.23@gmail.com>
2026-03-22 12:14:41 +0300

    maintain/dump-prepare.sh: added support for postgres

--- maintain/dump-prepare.sh
+++ maintain/dump-prepare.sh
@@ -19,6 +19,7 @@ if command -v docker >/dev/null 2>&1; then
 else
 
   # Shortcuts
+  engine="$(get_env "DB_ENGINE")"
   dir="${1:-data}"
   host="$DB_HOST"
   user="$DB_USER"
@@ -33,20 +34,37 @@ else
   # Trim .gz from dump filename
   sql=$(echo "$dump" | sed 's/\.gz$//')
 
-  # Put password into env to solve the warning message:
-  # 'mysqldump: [Warning] Using a password on the command line interface can be insecure.'
-  export MYSQL_PWD="$pass"
+  if [[ "$engine" == "mysql" ]]; then
+    pwdenv="MYSQL_PWD"
+    args="-h $host -u $user -N -e"
+    query="mysql $args"
+    tables='SHOW TABLES FROM `'$name'`'
+    recordQty="SELECT TABLE_ROWS FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '$name' AND TABLE_NAME = '---'"
+    dump_bin="mysqldump"
+    dump_cmd="$dump_bin -h $host -u $user -y $name --single-transaction"
+  elif [[ "$engine" == "postgres" ]]; then
+    pwdenv="PGPASSWORD"
+    args="-h $host -U $user -t -q -c"
+    query="psql $args"
+    tables="SELECT tablename FROM pg_tables WHERE schemaname = 'public'"
+    recordQty="SELECT reltuples::bigint FROM pg_class WHERE relname = '---'"
+    dump_bin="pg_dump"
+    dump_cmd="$dump_bin -h $host -U $user -d $name --no-owner --no-acl --inserts --rows-per-insert=1000"
+  fi
+
+  # Put password into env
+  export "${pwdenv}=${pass}"
 
   # Estimate export as number of records to be dumped
   msg="${pref}Calculating approximate qty of total rows..."; echo $msg
-  total=0; tables=0
-  args="-h $host -u $user -N -e"
-  for table in $(mysql $args 'SHOW TABLES FROM `'$name'`;'); do
-    count=$(mysql $args "SELECT TABLE_ROWS FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '$name' AND TABLE_NAME = '$table';")
+  total=0; tableQty=0
+  for table in $($query "$tables"); do
+    count="$($query "${recordQty/---/"$table"}")"
+    count=${count//[[:space:]]/}
     (( total+=count )) || true
-    (( tables++ )) || true
+    (( tableQty++ )) || true
     clear_last_lines 1
-    echo -n "$msg "; printf "%'d" "$total"; echo " in $tables tables"
+    echo -n "$msg "; printf "%'d" "$total"; echo " in $tableQty tables"
   done
 
   # Target gz file path
@@ -62,24 +80,22 @@ else
   # Export dump with printing progress
   [ -d "$dir" ] || mkdir -p "$dir"
   msg="${pref}Exporting $(basename "$gz") into $dir/ dir...";
-  mysqldump --single-transaction -h $host -u $user -y $name \
-  | tee >(grep --line-buffered '^INSERT INTO' \
-      | awk -v total="$total" -v msg="$msg" '{
-          count += gsub(/\),\(/, "&") + 1
-          percent = int((count / total) * 100)
-          if (percent != last) {
-            printf "\r%s %d / %d (%d%%)", msg, count, total, percent
-            fflush()
-            last = percent
-          }
-        }' >&2) \
+  $dump_cmd | tee >(grep --line-buffered '^INSERT INTO' | awk -v total="$total" -v msg="$msg" '{
+      count += gsub(/\),\(/, "&") + 1
+      percent = int((count / total) * 100)
+      if (percent != last) {
+        printf "\r%s %d / %d (%d%%)", msg, count, total, percent
+        fflush()
+        last = percent
+      }
+    }' >&2) \
   | gzip | split --bytes=${GH_ASSET_MAX_SIZE^^} --numeric-suffixes=1 - $gz
 
   # Unset from env
-  unset MYSQL_PWD
+  unset "$pwdenv"
 
   # Exit if above command failed
-  exit_code=$?; if [[ $exit_code -ne 0 ]]; then echo "mysqldump exited with code $exit_code"; exit $exit_code; fi
+  exit_code=${PIPESTATUS[0]}; if [[ $exit_code -ne 0 ]]; then echo "$dump_bin exited with code $exit_code"; exit $exit_code; fi
 
   echo ""
   clear_last_lines 1

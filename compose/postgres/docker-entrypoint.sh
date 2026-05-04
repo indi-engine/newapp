d2dd600b098185e2b2b3344ffd6ab57a8443081d
Pavel Perminov <pavel.perminov.23@gmail.com>
2026-05-04 23:12:02 +0300

    1.cleanup: added 'source cleanup cvl' command 2.compose/postgres/docker-entrypoint.sh: added touch and chown for postgresql.log

--- compose/postgres/docker-entrypoint.sh
+++ compose/postgres/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -eu -o pipefail
 
+# Create log file and set postgres to be it's owner
+log="/var/log/postgresql/postgresql.log"; touch "$log"; chown postgres:postgres "$log"
+
 # Make everything written to the stdout and stderr to be also written to a log file
 exec > >(tee -a /var/log/postgresql/container.log) 2>&1
 
